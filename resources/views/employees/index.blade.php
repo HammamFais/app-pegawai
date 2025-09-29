4fac336a039edd164f5af660cd9ba62e08f2ea9d
HammamFais <hammamfais7@gmail.com>
2025-09-29 18:24:17 +0700

    Penambahan Master Layout

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title>Daftar Pegawai</title>
-</head>
-
-<body>
+@extends('master')
+@section('title', 'Daftar Pegawai')
+@section('content')
     <div class="container mt-5">
         <h1 class="mb-4">Daftar Pegawai</h1>
         <table border="1" cellpadding="5" cellspacing="0">
@@ -46,6 +41,4 @@
             </tbody>
         </table>
     </div>
-</body>
-
-</html>
+@endsection
